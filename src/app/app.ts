7e9fa35fb4fc0bf574f91c80277bb0f1e3e32d00
mparra39 <mparrah39@gmail.com>
2025-09-18 15:56:15 -0600

    110 nebu superior e iconos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   //imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [RouterOutlet]
+  imports: [RouterOutlet, FooterComponent]
 })
 export class App {}
--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,11 +1,23 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { CountryLayoutComponent } from './layouts/CountryLayout/CountryLayout.component';
 
 export const countryRoutes: Routes = [
 
   {
     path: '',
-    component: ByCapitalPageComponent,
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent,
+      },
+
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      },
+    ]
   },
 
  // {
@@ -13,10 +25,6 @@ export const countryRoutes: Routes = [
  //   //??? {}
  // },
 
-  {
-    path: '**',
-    redirectTo: '',
-  },
 ];
 
 
