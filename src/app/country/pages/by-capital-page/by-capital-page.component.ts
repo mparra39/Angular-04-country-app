e0127f99ecd15d9ae122c33e67bf53125f0f64e9
mparra39 <mparrah39@gmail.com>
2025-09-23 21:44:13 -0600

    125 Buscar paises

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { CountryService } from '../../services/country.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -8,9 +10,42 @@ import { CountryListComponent } from "../../components/country-list/country-list
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
+  countryService = inject(CountryService);
+  query = signal('');
 
-  onSearch(value: string) {
-    console.log({value});
-  }
+  countryResource = resource({
+    params: () => ({ query: this.query() }),
+    loader: async({ params }) => {
+      if (!params.query) return [];
 
- }
+      return await firstValueFrom(
+        this.countryService.searchByCapital(params.query)
+      );
+    },
+  });
+
+      // isLoading = signal(false);
+      // isError = signal<string | null>(null);
+      // countries = signal<Country[]>([]);
+
+      // onSearch(query: string) {
+
+      //   if (this.isLoading()) return;
+
+      //   this.isLoading.set(true);
+      //   this.isError.set(null);
+
+      //   this.countryService.searchByCapital(query)
+      //     .subscribe({
+      //       next: (countries) => {
+      //           this.isLoading.set(false);
+      //           this.countries.set(countries);
+      //         },
+      //         error: (err) => {
+      //           this.isLoading.set(false);
+      //           this.countries.set([]);
+      //           this.isError.set(err);
+      //         }
+      //     });
+      // }
+}
