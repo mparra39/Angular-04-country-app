346a450bddc5ce01fa62bd95a89ada8da137597c
mparra39 <mparrah39@gmail.com>
2025-09-24 18:40:01 -0600

    127 rxResource

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -2,7 +2,9 @@ import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
-import { firstValueFrom } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { Country } from '../../interfaces/country.interface';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -13,16 +15,26 @@ export class ByCapitalPageComponent {
   countryService = inject(CountryService);
   query = signal('');
 
-  countryResource = resource({
-    params: () => ({ query: this.query() }),
-    loader: async({ params }) => {
-      if (!params.query) return [];
-
-      return await firstValueFrom(
-        this.countryService.searchByCapital(params.query)
-      );
+    countryResource = rxResource<Country[], { query: string }>({
+    params: () => ({ query: this.query().trim() }),
+    // En Angular 20 la clave es "stream" (antes "loader")
+    stream: ({ params }) => {
+      if (!params.query) return of([]);             // evita llamadas en vacío
+      return this.countryService.searchByCapital(params.query);
     },
+    defaultValue: [],                                // opcional pero útil
   });
+
+  // countryResource = resource({
+  //   params: () => ({ query: this.query() }),
+  //   loader: async({ params }) => {
+  //     if (!params.query) return [];
+
+  //     return await firstValueFrom(
+  //       this.countryService.searchByCapital(params.query)
+  //     );
+  //   },
+  // });
 
       // isLoading = signal(false);
       // isError = signal<string | null>(null);
